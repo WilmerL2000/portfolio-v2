0cc407d47ca36f444f58056a7775386f483e2ccd
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-11-06 18:39:21 -0600

    Style changed

--- portfolio/components/ui/project-header.tsx
+++ portfolio/components/ui/project-header.tsx
@@ -23,11 +23,11 @@ export default function ProjectHeader({
     <motion.header
       style={{ backgroundImage: `url(${projectImage})` }}
       layoutId={id}
-      className="rounded-3xl md:rounded-lg relative aspect-video min-h-[250px] md:max-h-[750px] w-full overflow-hidden bg-cover"
+      className="rounded-lg relative aspect-video min-h-[250px] md:max-h-[650px] w-full overflow-hidden bg-cover"
     >
       <motion.div
         layoutId={title}
-        className="flex justify-between items-center absolute bottom-2 md:bottom-1 left-0 right-0 
+        className="flex justify-between items-center absolute bottom-2 md:bottom-0 left-0 right-0 
           m-2 p-1 md:p-4 backdrop-blur-xl bg-black/30 rounded-lg"
       >
         <h1 className="font-bold text-lg sm:text-5xl text-white/70">{title}</h1>
